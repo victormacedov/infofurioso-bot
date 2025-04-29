75342516b0a0fc506a1d3f0158aed0b8aefea756
Victor Macêdo <victormacedo1001@gmail.com>
2025-04-29 12:05:02 -0300

    Update start.py
    Deixei o /start mais completo e informativo para quem for utilizar o bot pela primeira vez e precisa saber o que cada comando faz.

--- source/handlers/start.py
+++ source/handlers/start.py
@@ -2,4 +2,11 @@ from telegram import Update
 from telegram.ext import ContextTypes
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Fala, fã da FURIA! 🐾\nUse /escalacao, /resultado ou /proximo")
+    await update.message.reply_text(
+        "Fala, fã da FURIA! 🐾\n"
+        "Use:\n"
+        "/escalacao -> Mostra os jogadores das equipes da FURIA em todas as modalidades disponíveis / ativas.\n"
+        "/resultado -> Exibe os resultados das 5 últimas partidas dos times disponíveis / ativos.\n"
+        "/proximo -> Mostra os próximos jogos programados da FURIA dos times disponíveis / ativos."
+    )
+
